07d4214fdfb67b43798deae3f15ef03afe70d7b5
rramjuttun <47162900+rramjuttun@users.noreply.github.com>
2023-06-27 16:57:24 -0700

    buttons

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 const vscode = require('vscode');
 const { uploadOnly, uploadAndMint } = require('./ipfs-upload.js');
 const { processDotenv, makePath, checkArtifactDefault } = require('./helpers.js');
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -16,20 +17,25 @@ async function activate(context) {
 	checkArtifactDefault(context.extensionPath);
 
 	context.subscriptions.push(vscode.commands.registerCommand('upload-extension.upload', 
-		async function () {
+		async function (folderURI) {
 			vscode.window.showInformationMessage('Uploading folder to IPFS.');
-			const folderPath = makePath('./images');
-			const upload = await uploadOnly(folderPath, ipfsInstance);
-			console.log(`Folder Base URI: ipfs://${upload.Hash}`);
+			const upload = await uploadOnly(folderURI.path, ipfsInstance);
+			console.log(`Folder URI: ipfs://${upload.Hash}`);
 		})
 	);
 	
 	context.subscriptions.push(vscode.commands.registerCommand('upload-extension.uploadAndMint', 
-		async function () {
-			vscode.window.showInformationMessage('Uploading folder to IPFS and deploying smart contract.');
-			const folderPath = makePath('./images');
-			const jsonPath = makePath('./json');
+		async function (folderURI) {
+			//vscode.window.showInformationMessage('Uploading folder to IPFS and deploying smart contract.');
+			const folderPath = folderURI.path
+			const jsonPath = makePath(path.join('./json', path.basename(folderPath)));
 			const upload = await uploadAndMint(folderPath, jsonPath, ipfsInstance, chainInstance);
+		})
+	);
+
+	context.subscriptions.push(vscode.commands.registerCommand('upload-extension.testCommand', 
+		function(folderURI) {
+			console.log(folderURI.path);
 		})
 	);
 }
--- ipfs-upload.js
+++ ipfs-upload.js
@@ -6,66 +6,61 @@ const { deployContract } = require('./deploy.js');
 const { checkInputFolderOnlyImages, makePath } = require('./helpers.js')
 
 async function _uploadFolder(folderPath, ipfsInstance, returnAll=false) {
-    try {
-        const form = new FormData();
+    const form = new FormData();
 
-        async function traverseDirectory(dirPath, parentPath = '') {
-            const files = await fs.promises.readdir(dirPath);
-        
-            for (const file of files) {
-                const filePath = path.join(dirPath, file);
-                const stats = await fs.promises.stat(filePath);
-        
-                if (stats.isFile()) {
-                    const fileStream = fs.createReadStream(filePath);
-                    const relativePath = path.join(parentPath, file);
-        
-                    form.append('file', fileStream, {
-                    filename: encodeURIComponent(relativePath),
-                    filepath: `/${encodeURIComponent(relativePath)}`,
-                    contentType: 'application/octet-stream',
-                    });
-                } else if (stats.isDirectory()) {
-                    const relativePath = path.join(parentPath, file);
-                    form.append('file', '', {
-                    filename: relativePath,
-                    filepath: `/${relativePath}`,
-                    contentType: 'application/x-directory',
-                    });
-        
-                    await traverseDirectory(filePath, relativePath);
-                }
+    async function traverseDirectory(dirPath, parentPath = '') {
+        const files = await fs.promises.readdir(dirPath);
+    
+        for (const file of files) {
+            const filePath = path.join(dirPath, file);
+            const stats = await fs.promises.stat(filePath);
+    
+            if (stats.isFile()) {
+                const fileStream = fs.createReadStream(filePath);
+                const relativePath = path.join(parentPath, file);
+    
+                form.append('file', fileStream, {
+                filename: encodeURIComponent(relativePath),
+                filepath: `/${encodeURIComponent(relativePath)}`,
+                contentType: 'application/octet-stream',
+                });
+            } else if (stats.isDirectory()) {
+                const relativePath = path.join(parentPath, file);
+                form.append('file', '', {
+                filename: relativePath,
+                filepath: `/${relativePath}`,
+                contentType: 'application/x-directory',
+                });
+    
+                await traverseDirectory(filePath, relativePath);
             }
         }
-
-        await traverseDirectory(folderPath);
-
-        const ipfsEndpoint = ipfsInstance.endpoint;
-        const ipfsAuth = ipfsInstance.auth;
-        const response = await axios.post(ipfsEndpoint+"/api/v0/add", form, {
-            params: {
-                'wrap-with-directory': true,
-                'cid-version': 1,
-            },
-            headers: {
-                ...form.getHeaders(),
-                Authorization: ipfsAuth
-            }
-        });
-
-        const uploads = response.data.split('\n').filter((element) => element !== '');
-        return returnAll ? uploads : JSON.parse(uploads[uploads.length-1]);
-
-    } catch(error) {
-        console.error('Error uploading to IPFS:', error);
     }
+
+    await traverseDirectory(folderPath);
+
+    const ipfsEndpoint = ipfsInstance.endpoint;
+    const ipfsAuth = ipfsInstance.auth;
+    const response = await axios.post(ipfsEndpoint+"/api/v0/add", form, {
+        params: {
+            'wrap-with-directory': true,
+            'cid-version': 1,
+        },
+        headers: {
+            ...form.getHeaders(),
+            Authorization: ipfsAuth
+        }
+    });
+
+    const uploads = response.data.split('\n').filter((element) => element !== '');
+    return returnAll ? uploads : JSON.parse(uploads[uploads.length-1]);
 }
 
-function _createJSONFromIPFS(jsonDirectory, assets) {
+function _createJSONFromIPFS(jsonDirectory, assets) { 
     if(fs.existsSync(jsonDirectory)) {
         fs.rmSync(jsonDirectory, {recursive: true, force: true});
     }
-    fs.mkdirSync(jsonDirectory);
+    fs.mkdirSync(jsonDirectory, { recursive: true });
 
     const baseCID = JSON.parse(assets[assets.length-1]).Hash;
     //console.log(`DEBUG: Image base: ${baseCID}`);
@@ -82,23 +77,29 @@ function _createJSONFromIPFS(jsonDirectory, assets) {
         json.image = `ipfs://${baseCID}/${_path}`;
     
         fs.writeFileSync(`${jsonDirectory}/${i}`, JSON.stringify(json, null, 2));
-    } 
+    }
 }
 
 async function uploadAndMint(folderPath, jsonPath, ipfsInstance, chainInstance) {
-    const numImages = checkInputFolderOnlyImages(folderPath);
-    const uploadedAssets = await _uploadFolder(folderPath, ipfsInstance, returnAll=true);
+    try {
+        const numImages = checkInputFolderOnlyImages(folderPath);
+        const uploadedAssets = await _uploadFolder(folderPath, ipfsInstance, returnAll=true);
 
-    _createJSONFromIPFS(jsonPath, uploadedAssets);
-    const uploadedJSON = await _uploadFolder(jsonPath, ipfsInstance);
-    const baseURI = 'ipfs://' + uploadedJSON.Hash+ '/';
-    console.log(`JSON Base URI: ${baseURI}`);
+        _createJSONFromIPFS(jsonPath, uploadedAssets);
+        const uploadedJSON = await _uploadFolder(jsonPath, ipfsInstance);
+        const baseURI = 'ipfs://' + uploadedJSON.Hash+ '/';
+        console.log(`JSON Base URI: ${baseURI}`);
 
-    const artifactPath = makePath('artifacts/SimpleERC721.json')
-    const constructorArgs = ["Collection 10", "AMG", baseURI, numImages];
-    const contractAddress = await deployContract(chainInstance, artifactPath, constructorArgs);
-    console.log(`Contract deployed to ${contractAddress}`);
-    return(contractAddress);
+        const artifactPath = makePath('artifacts/SimpleERC721.json')
+        const constructorArgs = ["Collection 10", "AMG", baseURI, numImages];
+        const contractAddress = await deployContract(chainInstance, artifactPath, constructorArgs);
+        console.log(`Contract deployed to ${contractAddress}`);
+        return(contractAddress);
+    } catch(error) {
+        console.error(error.message)
+        process.exitCode = 1;
+    }
+    
 }
 
 async function uploadOnly(folderPath, ipfsInstance) {
